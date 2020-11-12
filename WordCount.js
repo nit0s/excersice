c400ece907b67b86c091458d8edbe6b8b0350adf
nit0's <41517082+nit0s@users.noreply.github.com>
2020-11-12 15:35:42 -0600

    adding code changes to wordCount file

--- WordCount.js
+++ WordCount.js
@@ -1,1 +1,39 @@
+const { time } = require("console");
+
+var originalString ='Hola mundo Hola mundo';
+var counter;
+var boxSaver = []
+var times = []
+
+function countWords(word){
+    word = word.split(" ");
+for(i= 0; i < word.length; i++){
+    counter = 0  
+    for(x=0; x < word.length; x++){
+              
+        if(word[i] == word[x] && (boxSaver.includes(word[i]) == false)){
+            counter+=1;
+            boxSaver.push(word[i])
+        }else{
+            if(word[i] == word[x]){ counter+=1 }
+
+        }
+    }
+   times.push(counter);
+}
+
+}//end of countWords
+
+function printResults(){
+    console.log('original String: '+originalString)
+    console.log('results: ')
+
+    for(i in boxSaver){
+        console.log(boxSaver[i]+": "+times[i]);
+    }
+
+}
+
+countWords(originalString);
+printResults();
 
