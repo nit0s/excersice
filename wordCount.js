8d739e3c76fd004d07b6b6d055ee17a49aabfdcf
nit0's <41517082+nit0s@users.noreply.github.com>
2020-11-12 19:39:20 -0600

    last format udpates

--- wordCount.js
+++ wordCount.js
@@ -11,11 +11,10 @@ function countWords(word){
       counter = 0  
       for(x=0; x < word.length; x++){
         if(word[i] == word[x] && (boxSaver.includes(word[i]) == false)){
-        counter+=1;
-        boxSaver.push(word[i])
+          counter+=1;
+          boxSaver.push(word[i])
          }else{
-            if(word[i] == word[x]){ counter+=1 
-               }
+            if(word[i] == word[x]){ counter+=1 }
           }  
        }  
     times.push(counter);
