fe1dd4667bffaa9f364f99ee95c564ee9e23ec08
nit0's <41517082+nit0s@users.noreply.github.com>
2020-11-12 19:32:34 -0600

    formatting changes and update class name

--- wordCount.js
+++ wordCount.js
@@ -7,20 +7,19 @@ var times = []
 
 function countWords(word){
     word = word.split(" ");
-for(i= 0; i < word.length; i++){
-    counter = 0  
-    for(x=0; x < word.length; x++){
-              
+    for(i= 0; i < word.length; i++){
+      counter = 0  
+      for(x=0; x < word.length; x++){
         if(word[i] == word[x] && (boxSaver.includes(word[i]) == false)){
-            counter+=1;
-            boxSaver.push(word[i])
-        }else{
-            if(word[i] == word[x]){ counter+=1 }
-
-        }
-    }
-   times.push(counter);
-}
+        counter+=1;
+        boxSaver.push(word[i])
+         }else{
+            if(word[i] == word[x]){ counter+=1 
+               }
+          }  
+       }  
+    times.push(counter);
+    }  
 
 }//end of countWords
 
